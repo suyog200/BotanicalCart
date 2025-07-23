cf8356f1e9e07c0043c930bc80ade3ef9e96c5c9
suyog chari <suygchari@gmail.com>
2025-07-23 11:08:02 +0530

    created Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar"
 
 
 const App = () => {
   return (
     <div>
-      hello
+      <Navbar />
     </div>
   )
 }
---
+++ frontend/src/context/AppContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Define the context type
+interface AppContextType {
+    navigate: ReturnType<typeof useNavigate>; //keeping the type as AppContextProvider will be used within BrowserRouter
+    user: any; // Replace 'any' with a more specific type if available
+    setUser: (user: any) => void; // Replace 'any' with a more specific type if available
+    seller: boolean;
+    setIsSeller: (seller: boolean) => void;
+    showUserLogin?: boolean;
+    setShowUserLogin?: (show: boolean) => void;
+}
+
+export const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const AppContextProvider = ({ children } : { children: React.ReactNode }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(true);
+    const [seller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+
+    const value = { navigate, user, setUser, seller, setIsSeller, showUserLogin, setShowUserLogin };
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+};    
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+};
